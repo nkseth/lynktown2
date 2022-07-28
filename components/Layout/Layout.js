b759256a8eb4b8a461936efb56e558c0fe7fda34
nanak <nanaksethh@gmail.com>
2022-07-28 17:47:57 +0530

    werqw

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,13 +1,26 @@
-import React from 'react';
+/* eslint-disable @next/next/next-script-for-ga */
+/* eslint-disable react/no-unescaped-entities */
+import React, { useEffect } from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import Head from 'next/head';
 
 const Layout = ({ children }) => {
+  useEffect(()=>{
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)};
+    gtag('js', new Date());
+    gtag('config', 'G-TTP5FCW4FD');
+  },[])
   return (
     <>
       <Head>
         <title>U2Lynk Town</title>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TTP5FCW4FD"></script>
+<meta name="google-site-verification" content="2yo2OS7PnxSDhBmhGnE54wC83Lsl4doztBNsbMaxpds" />
+
+
         <meta
           name='description'
           content='A Revolutionary way of social Buying & Selling'
